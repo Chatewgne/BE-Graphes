f4a2efeb03bfffd33428d3a0932b7de15230afac
Celia Prat <chatewgne@gmail.com>
2018-03-30 10:51:31 +0000

    Dijkstra algorithm

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,15 @@
 package org.insa.algo.shortestpath;
 
+import org.insa.algo.utils.BinaryHeap;
+import org.insa.graph.Arc;
+import org.insa.graph.Graph;
+import org.insa.graph.Node;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import static jdk.nashorn.internal.objects.NativeMath.min;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -10,7 +20,42 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        // Retrieve the graph.
+        Graph graph = data.getGraph();
+        final int nbNodes = graph.size();
+        boolean done = false ;
+        // Initialize array of distances.
+        Label[] labels = new Label[nbNodes];
+        Arrays.fill(labels, new Label(false, null,Double.POSITIVE_INFINITY);
+        labels[data.getOrigin().getId()] = new Label(false,null,0.0);
+
+        BinaryHeap<Node> tas = new BinaryHeap<>();
+        tas.insert(data.getOrigin());
+
+        while (!done)
+        {
+            Node x = tas.deleteMin() ;
+            labels[x.getId()].marked = true;
+            Iterator<Arc> it = x.iterator();
+            while (it.hasNext())
+            {
+                Arc arc = it.next();
+                if (!(labels[arc.getDestination().getId()].marked))
+                {
+                    double AncienCout = labels[arc.getDestination().getId()].cost;
+                    double NewCout =labels[arc.getOrigin().getId()].cost + arc.getLength();
+                    if (AncienCout > NewCout)
+                    {
+                        labels[arc.getDestination().getId()].cost = NewCout;
+                        tas.insert(x);
+                        labels[arc.getDestination().getId()].parent = arc.getOrigin();
+                    }
+                }
+            }
+        }
+
+
+
         return solution;
     }
 
---
+++ src/main/org/insa/algo/shortestpath/Label.java
@@ -0,0 +1,15 @@
+package org.insa.algo.shortestpath;
+
+import org.insa.graph.Node;
+
+public class Label {
+    public boolean marked ;
+    public Node parent ;
+    public double cost ;
+
+    public Label(boolean marked, Node parent, double cost) {
+        this.marked = marked;
+        this.parent = parent;
+        this.cost = cost;
+    }
+}
