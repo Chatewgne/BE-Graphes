ebbf7c08506a9bdd7b87ca9c230f3eeff7beaa9a
Paul Florence <florencepaul@hotmail.fr>
2018-04-05 10:37:34 +0200

    Reconstruction du chemin du djikstra.

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -4,7 +4,9 @@ import org.insa.algo.utils.BinaryHeap;
 import org.insa.graph.Arc;
 import org.insa.graph.Graph;
 import org.insa.graph.Node;
+import org.insa.graph.Path;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 
@@ -25,6 +27,7 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         final int nbNodes = graph.size();
         boolean done = false ;
         // Initialize array of distances.
+        System.out.println("NbNodes : " + nbNodes);
         Label[] labels = new Label[nbNodes];
         Arrays.fill(labels, new Label(false, null,Double.POSITIVE_INFINITY));
         labels[data.getOrigin().getId()] = new Label(false,null,0.0);
@@ -35,32 +38,51 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         while (!done)
         {
             Node x = tas.deleteMin() ;
+            System.out.println("ID : " + x.getId());
             labels[x.getId()].marked = true;
             Iterator<Arc> it = x.iterator();
             while (it.hasNext())
             {
                 Arc arc = it.next();
-                if (!(labels[arc.getDestination().getId()].marked))
+                Node y = arc.getDestination();
+                if (!(labels[y.getId()].marked) && !y.equals(x))
                 {
-                    double AncienCout = labels[arc.getDestination().getId()].cost;
-                    double NewCout =labels[arc.getOrigin().getId()].cost + arc.getLength();
+                    double AncienCout = labels[y.getId()].cost;
+                    double NewCout = labels[arc.getOrigin().getId()].cost + arc.getLength();
                     if (AncienCout > NewCout)
                     {
-                        labels[arc.getDestination().getId()].cost = NewCout;
-                        tas.insert(x);
-                        labels[arc.getDestination().getId()].parent = arc.getOrigin();
+                        labels[y.getId()].cost = NewCout;
+                        tas.insert(y);
+                        labels[y.getId()].parent = arc;
+                        labels[y.getId()].marked = false;
                     }
                 }
             }
             done = true;
             for (Label lab : labels)
             {
-                if (lab.marked) {
+                if (!lab.marked) {
                     done = false;
                 }
             }
         }
 
+
+        ArrayList<Arc> result = new ArrayList<Arc>();
+
+        int debut = data.getDestination().getId();
+        boolean done_rebuilding = false;
+        while (! done_rebuilding) {
+            Label current = labels[debut];
+            Arc path_chunk = current.parent;
+            result.add(path_chunk);
+            debut = path_chunk.getOrigin().getId();
+
+            if (path_chunk.getOrigin().equals(data.getOrigin())) {
+                done_rebuilding = true;
+            }
+
+        }
 
 
         return solution;
--- src/main/org/insa/algo/shortestpath/Label.java
+++ src/main/org/insa/algo/shortestpath/Label.java
@@ -1,13 +1,14 @@
 package org.insa.algo.shortestpath;
 
+import org.insa.graph.Arc;
 import org.insa.graph.Node;
 
 public class Label {
     public boolean marked ;
-    public Node parent ;
+    public Arc parent ;
     public double cost ;
 
-    public Label(boolean marked, Node parent, double cost) {
+    public Label(boolean marked, Arc parent, double cost) {
         this.marked = marked;
         this.parent = parent;
         this.cost = cost;
