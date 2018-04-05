74384593324e642f4cc76c6bec0bae6a5922e607
Paul Florence <florencepaul@hotmail.fr>
2018-04-05 12:07:24 +0200

    Gestion des chemins infaisables.

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -79,32 +79,31 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
         ArrayList<Node> result = new ArrayList<Node>();
 
-        /* Création du chemin */
-        Node current = data.getDestination();
-        boolean done_rebuilding = false;
-        while (! done_rebuilding) {
-            result.add(current);
-            current = labels.get(current.getId()).parent;
-
-            if (current.equals(data.getOrigin())) {
-                done_rebuilding = true;
-            }
-        }
-
-        /* Inversion du chemin */
-        for(int i = 0, j = result.size() - 1; i < j; i++) {
-            result.add(i, result.remove(j));
-        }
-
         try {
-            Path sol_path = Path.createShortestPathFromNodes(graph, result);
-            solution = new ShortestPathSolution(data, AbstractSolution.Status.FEASIBLE, sol_path);
-            return solution;
-        }
-        catch (Exception e) {
-            return new ShortestPathSolution(data, AbstractSolution.Status.INFEASIBLE, null);
+            /* Création du chemin */
+            Node current = data.getDestination();
+            boolean done_rebuilding = false;
+            while (! done_rebuilding) {
+                result.add(current);
+                current = labels.get(current.getId()).parent;
+
+                if (current.equals(data.getOrigin())) {
+                    done_rebuilding = true;
+                }
+            }
+
+                /* Inversion du chemin */
+                for(int i = 0, j = result.size() - 1; i < j; i++) {
+                    result.add(i, result.remove(j));
+                }
+                Path sol_path = Path.createShortestPathFromNodes(graph, result);
+                solution = new ShortestPathSolution(data, AbstractSolution.Status.FEASIBLE, sol_path);
+                return solution;
         }
 
+        catch (Exception e) {
+                return new ShortestPathSolution(data, AbstractSolution.Status.INFEASIBLE, null);
+        }
     }
 
 }
